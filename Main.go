732be1817ccb62e386f5c0289549a2fab9eaafec
gui <guisolon.tec@gmail.com>
2025-09-22 21:30:32 -0300

    Adicionando um arquivo go e aprendendo a mexer com a sintaxe

---
+++ Main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	
+	var idade int = 18
+	var nome string = "Carlos"
+
+
+	var altura float64
+	altura = 1.75
+
+	fmt.Println(idade)
+	fmt.Println(nome)
+	fmt.Println(altura)
+
+
+}
+
